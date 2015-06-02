bfebf958a5b46ac9a2bf1d6c4c3ca41068611cd1
AL02F13 <damaris.aniceto.marin@gmail.com>
2015-06-02 13:47:53 -0500

    tres
    configuracion de audio

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,14 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	audio=window.plugins.LowLatencyAudio;
+	audio.prreloadFX('B1','audio/C.mp3',function(){},function(msg){alert("Error "+msg);});
+	
+	audio.prreloadFX('B2','audio/D.mp3',function(){},function(msg){alert("Error "+msg);});
+	
+	audio.prreloadFX('B3','audio/E.mp3',function(){},function(msg){alert("Error "+msg);});
+	
+	audio.prreloadFX('B4','audio/F.mp3',function(){},function(msg){alert("Error "+msg);});
 	$('#btnjugar').on('tap',function(){
 		var pantalla=$.mobile.getScreenHeight();
 		var encabezado=$('.ui-header').outerHeight();
@@ -25,7 +33,8 @@ $('.cuadro').on('vmouseup',function(){
 			
 });
 function quien(q)
-{
+{ 
+ audio.play(q);
 	return q.substring(1);
 }
 
